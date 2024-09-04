69003cfcd620c16bf2e793b879a94cc43d9b5fde
tuanlee <leevantuan2002@gmail.com>
2024-09-04 21:59:28 +0700

    [SWAGGER GEN] Setup SwaggerGen use Bearer

--- src/BadmintonSystem.API/DependencyInjection/Extensions/SwaggerExtensions.cs
+++ src/BadmintonSystem.API/DependencyInjection/Extensions/SwaggerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using BadmintonSystem.API.DependencyInjection.Options;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -10,7 +11,36 @@ public static class SwaggerExtensions
     public static void AddSwagger(this IServiceCollection services)
     {
         // Add Sawagger Gen in Program to here
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Smart", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement(){{ new OpenApiSecurityScheme {
+                                                                    Reference = new OpenApiReference
+                                                                        {
+                                                                        Type = ReferenceType.SecurityScheme,
+                                                                        Id = "Bearer"
+                                                                        },
+                                                                        Scheme = "oauth2",
+                                                                        Name = "Bearer",
+                                                                        In = ParameterLocation.Header,
+                                                                    },
+                                                                    new List<string>()
+                                                                    }
+                                                                });
+        });
+
         // Config thêm cho SwaggerGen
         services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
     }
